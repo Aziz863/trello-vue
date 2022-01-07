a74576f03b08666698281a974f707b159b4164b0
aziz <aziz.r8@hotmail.com>
2022-01-07 12:52:45 +0100

    First commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <span> Footer de la app de adidouh</span>
+    <router-link class="btn-header" to="/">Mis paneles</router-link>
+  </footer>
+</template>
+
+<script>
+export default { 
+    name: 'app-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  box-sizing: border-box;
+  background-color: #cfd8dc;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  position: absolute;
+  width: 100%;
+}
+
+</style>
